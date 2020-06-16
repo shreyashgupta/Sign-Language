a61aaa0b947345d0e17a0d2952b6acdd8682b73a
shreyashgupta <shreyashgupta88@gmail.com>
2020-06-17 01:00:46 +0530

    adding hull

--- main.py
+++ main.py
@@ -74,9 +74,20 @@ while capture.isOpened():
         drawing = np.zeros(crop_image.shape, np.uint8)
         cv2.drawContours(drawing, [contour], -1, (0, 255, 0), 0)
         cv2.drawContours(drawing, [hull], -1, (0, 0, 255), 0)
-
-        # Find convexity defects
         hull = cv2.convexHull(contour, returnPoints=False)
+        defects = cv2.convexityDefects(contour, hull)
+        for i in range(defects.shape[0]):
+            s, e, f, d = defects[i, 0]
+            start = tuple(contour[s][0])
+            end = tuple(contour[e][0])
+            far = tuple(contour[f][0])
+
+            a = math.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
+            b = math.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
+            c = math.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
+            angle = (math.acos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c)) * 180) / 3.14
+
+            cv2.line(crop_image, start, end, [0, 255, 0], 2)
 
     except:
         pass
